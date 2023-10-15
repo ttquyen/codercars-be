72fe6906ab20b6ef8c057831e6e8b4b0f69d8d93
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-10-15 17:58:32 +0700

    :construction: add src

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,95 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
+const { AppError } = require("../helpers/utils");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+    try {
+        // YOUR CODE HERE
+        const carInfo = req.body;
+        if (!carInfo) {
+            throw new AppError(401, "Bad Request", "Create Car Error");
+        }
+        const newCar = await Car.create(carInfo);
+        res.status(200).send({
+            car: newCar,
+            message: "Create Car Successfully!",
+        });
+    } catch (err) {
+        // YOUR CODE HERE
+        next(err);
+    }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+    try {
+        // YOUR CODE HERE
+
+        let { page, limit } = req.query;
+        page = parseInt(page) || 1;
+        limit = parseInt(limit) || 10;
+
+        //Number of items skip for selection
+        let offset = limit * (page - 1);
+
+        const filter = {};
+        //mongoose query
+        const listOfFound = await Car.find(filter).skip(offset).limit(limit);
+        // console.log(listOfFound);
+        const count = await Car.countDocuments({});
+        const totalPage = Math.ceil(count / limit);
+
+        res.status(200).send({
+            cars: listOfFound,
+            total: totalPage,
+            page: page,
+            message: "Get Car List Successfully!",
+        });
+    } catch (err) {
+        next(err);
+        // YOUR CODE HERE
+    }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+    try {
+        // YOUR CODE HERE
+        const targetId = req.params.id;
+        const updateInfo = req.body;
+        const options = { new: true };
+
+        const updated = await Car.findByIdAndUpdate(
+            targetId,
+            updateInfo,
+            options
+        );
+        res.status(200).send({
+            car: updated,
+            message: "Update Car Successfully!",
+        });
+    } catch (err) {
+        // YOUR CODE HERE
+        next(err);
+    }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+    try {
+        // YOUR CODE HERE
+        const targetId = req.params.id;
+        // const options = { new: true };
+
+        // const updated = await Car.findByIdAndDelete(targetId, options);
+        const updated = await Car.findByIdAndUpdate(targetId, {
+            isDeleted: true,
+        });
+        res.status(200).send({
+            car: updated,
+            message: "Delete Car Successfully!",
+        });
+    } catch (err) {
+        // YOUR CODE HERE
+    }
 };
 
 module.exports = carController;
---
+++ convert.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+const convertData = async () => {
+    let newData = await csv().fromFile("data.csv"); //return array of JSON
+    newData = new Set(newData.map((e) => e));
+    newData = Array.from(newData);
+    let data = JSON.parse(fs.readFileSync("db.json"));
+    newData = newData.map((car) => ({
+        make: car.Make,
+        model: car.Model,
+        transmission_type: car["Transmission Type"],
+        size: car["Vehicle Size"],
+        style: car["Vehicle Style"],
+        price: parseInt(car.MSRP),
+        isDeleted: false,
+    }));
+    data = newData;
+    fs.writeFileSync("db.json", JSON.stringify(data));
+};
+convertData();
