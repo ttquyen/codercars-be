7244b1b7a39121d17e99923edb313683c4a4f40f
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-10-16 11:31:12 +0700

    :bug: catch error when connecting to DB

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
-});
+/* DB connection*/
+const mongoURI = process.env.MONGO_URI;
+//Connect Mongo
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log(`DB connected ${mongoURI}`))
+    .catch((err) => console.log(err));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
